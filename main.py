75cd37bbb2d39b3aa147cfc72e4b29ffcba0613a
oHybrid <trungthacao@hotmail.com>
2013-08-04 14:27:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import ch 
+import random
+import sys
+import re
+if sys.version_info[0] > 2:
+    import urllib.request as urlreq
+else:
+    import urllib2 as urlreq
+    dictionary = dict() #volatile... of course...
+
+dancemoves = [
+     "(>^.^)>",
+     "(v^.^)v",
+     "v(^.^v)",
+     "<(^.^<)"
+class TestBot(ch.RoomManager):
+    def onInit(self):
+        ...shitandstuff
+        if __name__ == "__main__": TestBot.easy_start()
